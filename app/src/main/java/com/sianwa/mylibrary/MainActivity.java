b9491ddcbde6aa206bc0607a6f6a5bb6c91a5f31
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2022-11-06 12:27:27 +0300

    Initial commit

---
+++ app/src/main/java/com/sianwa/mylibrary/MainActivity.java
@@ -0,0 +1,38 @@
+package com.sianwa.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWantToRead, btnCurrentlyReading, btnFavourite, btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWantToRead = findViewById(R.id.btnWantToRead);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnFavourite = findViewById(R.id.btnFavourite);
+        btnAbout = findViewById(R.id.btnAbout);
+    }
+}
